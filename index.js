e75b02d2aadd02216fa45e9e4f24a01cc68c4fb0
Kristina Gromova <christina28romova@yandex.ru>
2020-10-07 23:14:14 +0300

    готово!

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-function save (event) {
-    event.preventDefault;
-    licenseValue = event.target.value;
-    return licenseValue;
-}
-
-function counter(plan, price) {
-    const result = plan * price;
-    document.querySelector('.total__result').textContent = `$${result}`;
-
-}
+let radios = document.querySelectorAll('input[type="radio"]');
 function result(event) {
     event.preventDefault;
-    document.querySelector('.total__result_select').textContent = `Selected plan: ${1}`
-    licensePrice = event.target.value;
-   
-    return counter(licenseValue, licensePrice);  
+    for (let radio of radios) {
+        if (radio.checked) {
+            let plan = radio.value;
+            let price = document.querySelector('.numder-license__select').value;
+            const result = plan * price;
+            document.querySelector('.total__result').textContent = `$${result}`;
+            selectPlan(plan);
+        }
+    }  
 }
-document.querySelector('.license-plan').addEventListener('click', save)
-document.querySelector('.numder-license__select').addEventListener('click', result)
+
+function selectPlan(plan) {
+    const selectContainer = document.querySelector('.total__result_select');
+if (plan == 13) {
+    selectContainer.textContent = 'Selected plan: #1';
+}
+else if (plan == 22) {
+    selectContainer.textContent = 'Selected plan: #2';
+}
+else {
+    selectContainer.textContent = 'Selected plan: #3';
+}
+}
+document.querySelector('.license-plan').addEventListener('click', result);
+document.querySelector('.numder-license__select').addEventListener('click', result);   
+   
